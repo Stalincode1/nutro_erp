4a453cc59e9654bb643f489e3eed606207cb8779
yogithyogan <yogithyogan2004x@gmail.com>
2024-09-14 17:57:08 +0530

    Categories

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:client/screens/user/Categories.dart';
 import 'package:client/screens/user/user_dashboard.dart';
+import 'package:client/screens/user/wish_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:client/routing/routes.dart';
 import 'package:client/screens/user/user_orders_screen.dart';
@@ -9,7 +11,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-  String defaultRoute = UserDashboard.routeName;
+  String defaultRoute = Categories.routeName;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
