7eee8a2ef92de038af6b758c0c70bace466855ab
BagioP <114640185+bxgio@users.noreply.github.com>
2024-09-14 14:40:51 +0530

    products added

---
+++ server/src/main/java/com/nutro/nutro_delivery/domain/CategoryEnum.java
@@ -0,0 +1,8 @@
+package com.nutro.nutro_delivery.domain;
+
+public enum CategoryEnum {
+    FRUITS,
+    NUTS,
+    DRY_FRUITS,
+    DATES
+}
---
+++ server/src/main/java/com/nutro/nutro_delivery/dto/response/ProductResponseDto.java
@@ -0,0 +1,69 @@
+package com.nutro.nutro_delivery.dto.response;
+
+public class ProductResponseDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private Integer currentStock;
+    private Double price;
+    private String images;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getCurrentStock() {
+        return currentStock;
+    }
+
+    public void setCurrentStock(Integer currentStock) {
+        this.currentStock = currentStock;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    // Getters and Setters
+}
