b84bd72e7564bbc2b2d413da9f30707e378cbbd7
Stalincode1 <98099014+Stalincode1@users.noreply.github.com>
2024-09-16 22:03:06 +0530

    Take pull for add To Cart

--- client/lib/config/app_config.dart
+++ client/lib/config/app_config.dart
@@ -1,5 +1,7 @@
 class AppConfig {
   //static const String baseUrl = "http://192.168.1.42:8080/api/v1";
-  static const String baseUrl = "http://192.168.1.42:8080";
+  // static const String baseUrl = "http://192.168.1.42:8080";
+  static const String baseUrl =
+      "https://09d2-2409-40f4-10f8-a795-8071-9e49-1503-e06a.ngrok-free.app";
   static const String oauthAPI = "/auth";
 }
